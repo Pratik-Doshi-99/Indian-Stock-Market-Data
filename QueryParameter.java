df4aff15e08921d8d8378a7f507550198bf9e429
Pratik Doshi <51810841+Pratik-Doshi-99@users.noreply.github.com>
2020-04-28 15:54:04 +0530

    Added Adjustment factor
    The Earlier version involved a bug such that if the end date was passed as 31-3-2020, The prices returned will be till 29-3-2020. The last day was excluded. The -1 on line 115 was causing the above issue. The -1 earlier was to naturally include the start date as the yahoo finance database ignore PERIOD1 by design.
    
    Moreover, it seems that yahoo finance no longer requires the cookies to validate a session before it provides the data. So the cookies and crumb is empty in version 2.0

--- QueryParameter.java
+++ QueryParameter.java
@@ -1,4 +1,3 @@
-
 package core;
 
 import java.util.logging.Level;
@@ -8,16 +7,14 @@ import java.util.logging.Logger;
  *
  * @author Pratik Doshi
  */
-
-/*This class is the most important part of program as it builds
- the query and houses the access cookies*/
-
 public class QueryParameter {
     private final long DATE_REMAINDER = 66600;
     private final int [] MONTH_DAYS = {31,28,31,30,31,30,31,31,30,31,30,31};
     
     private final String BASIC_QUERY = "https://query1.finance."
             + "yahoo.com/v7/finance/download/";
+    
+    //Other non user url query parameters
     
     private final String EVENTS = "history";
     
@@ -30,15 +27,15 @@ public class QueryParameter {
     private String CRUMB;
     
     private QueryParameter(){
-    	//constructor for testing code    
+        
     }
     
     
-    QueryParameter(String script, Date startDate, Date endDate, Interval interval) 
+    public QueryParameter(String script, Date startDate, Date endDate, Interval interval) 
             throws InvalidDateException{
         this.SYMBOL = script.trim();
-        this.PERIOD1 = this.getDate(startDate);
-        this.PERIOD2 = this.getDate(endDate);
+        this.PERIOD1 = this.getDate(startDate,-1);
+        this.PERIOD2 = this.getDate(endDate,0);
         if(PERIOD2<PERIOD1) throw new InvalidDateException("End date cannot"
                 + " occur after start date");
         this.setInterval(interval);
@@ -80,13 +77,15 @@ public class QueryParameter {
         return symbol;
     }
     public void setStartDate(Date start) throws InvalidDateException{
-        this.PERIOD1 = this.getDate(start);
+        this.PERIOD1 = this.getDate(start,-1);
         if(PERIOD2<PERIOD1) throw new InvalidDateException("End date cannot"
                 + " occur after start date");
         this.updateRequest();
+        String x = "https://query1.finance.yahoo.com/v7/finance/download/ACC.NS?period1=1556123325&period2=1587745725&interval=1d&events=history";
+        x = "https://query1.finance.yahoo.com/v7/finance/download/BANKNIFTY,,.NS?period1=1556044200&period2=1587753000&interval=1d&events=history&crumb=";
     }
     public void setEndDate(Date end) throws InvalidDateException{
-        this.PERIOD2 = this.getDate(end);
+        this.PERIOD2 = this.getDate(end,0);
         if(PERIOD2<PERIOD1) throw new InvalidDateException("End date cannot"
                 + " occur after start date");
         this.updateRequest();
@@ -99,6 +98,7 @@ public class QueryParameter {
                 "&interval=" + INTERVAL + 
                 "&events=" + EVENTS +
                 "&crumb=" + CRUMB;
+        
         return query;
                 
     }
@@ -108,16 +108,16 @@ public class QueryParameter {
     
     
     
-    private long getDate(Date d2){
+    private long getDate(Date d2, int adjFactor){
+        //returns the number of days since epooch
         Date d1 = new Date(1,1,1970);
         long days = this.yearCount(d1.getYear(), d2.getYear()) +
                 this.tillYearEnd(d1.getDay(), d1.getMonth(), d1.getYear()) +
-                this.fromYearStart(d2.getDay(), d2.getMonth(), d2.getYear()) - 1;
+                this.fromYearStart(d2.getDay(), d2.getMonth(), d2.getYear()) + adjFactor;
         long seconds = days * 86400  +  this.DATE_REMAINDER;
         return seconds;
         
     }
-
     private int yearCount(int y1, int y2){
         int dayCount = 0;
         for(int i =y1+1;i<y2;i++){
@@ -126,7 +126,6 @@ public class QueryParameter {
         }
         return dayCount;
     }
-
     private int tillYearEnd(int d, int m, int y){
         int dayCount = MONTH_DAYS[m-1] - d;
         for (int i = m; i < MONTH_DAYS.length; i++) {
@@ -139,7 +138,6 @@ public class QueryParameter {
         
         return dayCount;
     }
-
     private int fromYearStart(int d, int m, int y){
         int dayCount = d;
         for (int i = 0; i < (m-1); i++) {
@@ -152,10 +150,11 @@ public class QueryParameter {
     }
     
     private void updateRequest(){
-	/* Cookie, and crumb values have been manually taken from the browser. If this code is used in the future,
-	   the cookie and crumb values will have to be updated has they have a life of 1 year.
-	   Alternatively, a system that maintains cookies in a file and regularly updates them will have to be set up. */
-
+        //GIVE ACTUAL COOKIE VALUES TAKEN FROM BROWSER
+        //this method will later have to be updated to access cookie data from a file
+        //when this is actually implemented on server side. The number of cookies needed will be much more than
+        //10 (as in this case). there will have to be a system in place that will keep track of cookie expiry and 
+        // create new cookies (even if it is manually)
         String[] cookie = {"APID=UPe5270c4a-9f42-11e9-8374-06ba32cdb226;"
                     + " PRF=t%3DSBIN.NS%252BHDFCBANK.NS;"
                     + " APIDTS=1562391147;"
@@ -164,11 +163,14 @@ public class QueryParameter {
                         "APID=UPd4ad9cf9-a646-11e9-b281-064dcd50edee;"
                     + " PRF=t%3DINR%253DX%252BSBIN.NS%252BJSWSTEEL.NS%252BHDFCBANK.NS;"
                     + " APIDTS=1563627011;"
-                    + " B=6sshhtleimga9&b=3&s=6f"};
+                    + " B=6sshhtleimga9&b=3&s=6f",
+        
+                        "B=60n8si1f7k57g&b=3&s=v5;"
+                    + " PRF=t%3DKOTAKBANK.NS"};
         String[] crumb = {"Dl4ap4VvgK5","8LHMipHkVdb"};        
         
         
-        //code that randomly selects a cookie and crumb from the array
+        
         double d= Math.random();
         int i = (int)(d*10000);
         i = i%(10^cookie.length);
@@ -178,11 +180,14 @@ public class QueryParameter {
             i = i%(10^cookie.length);
         }
         
-        this.COOKIE = cookie[i];
-        this.CRUMB = crumb[i];
+        this.COOKIE = "";//cookie[1];
+        this.CRUMB = "";//crumb[i];
     }
+    
+    
+    //create methods to allow user of this class to change parameters
          
-    //Testing the code
+    
     public static void main(String[] args) {
         try {
             QueryParameter param = new QueryParameter("SBIN.NS",
@@ -194,7 +199,12 @@ public class QueryParameter {
             System.out.println(ex.getMessage());
         }
         
+        test();
         
-        
+    }
+    
+    public static void test(){
+        String s = "ABCD" + (6+1);
+        System.out.println(s);
     }
 }
